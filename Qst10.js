267e33e38360567256a444cd118f5d17f8464e8f
Filipe <104007477+filipecarv@users.noreply.github.com>
2024-04-08 23:19:47 -0300

    Delete Qst10.js

--- Qst10.js
+++
@@ -1,8 +0,0 @@
-let Random1 = Math.random();
-console.log(Random1);
-
-let Random2 = parseInt(Math.random() * 10) + 1;
-console.log(Random2);
-
-let Random3 = parseInt(Math.random() * 1000) + 1;
-console.log(Random3);
