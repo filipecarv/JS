6cd63882b426c374911d51b13a3660bde23ba78f
Filipe <104007477+filipecarv@users.noreply.github.com>
2024-04-08 23:19:55 -0300

    Delete Qst11.js

--- Qst11.js
+++
@@ -1,8 +0,0 @@
-Dia = prompt('Que dia e hj meu mano!!');
-if (Dia == 'Sábado') {
-    alert('Bom fim de semana pra tu meu mano!!');
-} else if (Dia == 'Domingo') {
-    alert('Bom fim de semana pra tu meu mano!!');
-} else {
-    alert('Boa semana pra tu meu mano!!');
-}
